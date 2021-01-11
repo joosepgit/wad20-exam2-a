5c797e75e97c4ecd7d229f4ebde994cc379634ed
joosepgit <tavitsjoosep@gmail.com>
2021-01-11 15:02:24 +0200

    Finished tasks

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,13 +2,28 @@
     <table>
         <tr>
             <td>From / To</td>
-            <td id="amount-label">Amount</td>
+            <td v-on:click="toggleSort()" id="amount-label">Amount</td>
             <td>Currency</td>
         </tr>
+        <tr v-for="(item, index) in transactions"
+          class="row"
+          :class="{income: item.type === 'income', spending: item.type === 'spending'}"
+          :key="index">
+          <td>
+            <span v-if="item.type === 'income'">{{item.from}}</span>
+            <span v-else>{{item.to}}</span>
+          </td>
+          <td>
+          <span> {{item.amount}}</span>
+          </td>
+          <td>
+            <span>{{item.currency}}</span>
+          </td>
+        </tr>
         <tr>
-            <td colspan="3" style="text-align: right;">
-                <span>Balance: <span id="balance"></span> EUR</span>
-            </td>
+          <td colspan="3" style="text-align: right;">
+            <span>Balance: {{ balance }}<span id="balance"></span> EUR</span>
+          </td>
         </tr>
     </table>
 </template>
@@ -18,6 +33,43 @@
         name: 'HelloWorld',
         props: {
             transactions: Array
+        },
+        created() {
+          this.transactions = this.sortedTransactions;
+          this.oldestFirst = false;
+        },
+        methods: {
+          toggleSort: function() {
+            this.oldestFirst = !this.oldestFirst;
+            let sortedList = this.transactions;
+            let order = this.oldestFirst;
+            sortedList.sort(function(a, b) {
+              if (order) {
+                return b.amount - a.amount;
+              } else {
+                return a.amount - b.amount;
+              }
+            });
+            return sortedList;
+          }
+        },
+        computed: {
+          balance: function () {
+            let balance = 0;
+
+            for (let i of this.transactions) {
+              if (i.type === 'income') {
+                balance += i.amount;
+              } else {
+                balance -= i.amount;
+              }
+            }
+            return balance;
+          },
+
+          sortedTransactions: function () {
+            return this.toggleSort();
+          }
         }
     }
 </script>
@@ -27,5 +79,11 @@
     table {
         width: 50%;
         margin: 0 auto;
+    }
+    tr.income {
+      color: green;
+    }
+    tr.spending {
+      color: red;
     }
 </style>
